e1b89470e82e2a2f48f4cebee583a31595617119
Florian Bruggisser <florian@nexpose.ch>
2017-04-08 12:06:02 +0200

    optimized imports and added java doc

--- src/com/thomasdiewald/PS3Eye/PS3EyeCamera.java
+++ src/com/thomasdiewald/PS3Eye/PS3EyeCamera.java
@@ -2,11 +2,12 @@ package com.thomasdiewald.PS3Eye;
 
 import processing.core.PApplet;
 import processing.core.PConstants;
-import processing.core.PGraphics;
 import processing.core.PImage;
 
 /**
- * Created by cansik on 08.04.17.
+ * PS3 Eye Camera
+ *
+ * Implemented by Florian Bruggisser (https://github.com/cansik)
  */
 public class PS3EyeCamera implements PConstants {
     private PApplet parent;
@@ -15,6 +16,11 @@ public class PS3EyeCamera implements PConstants {
     private boolean autoRead = true;
     private boolean running = false;
 
+    /**
+     * Create a new PS3 Eye Camera.
+     * @param parent Parent Processing sketch.
+     * @param ps3Eye PS3 Eye Camera reference.
+     */
     public PS3EyeCamera(PApplet parent, PS3Eye ps3Eye) {
         this.parent = parent;
 
@@ -27,23 +33,35 @@ public class PS3EyeCamera implements PConstants {
         cam.init();
     }
 
+    /**
+     * Disposes the camera and cleans up the cache.
+     */
     public void dispose() {
         parent.g.removeCache(frame);
         cam.dispose();
     }
 
+    /**
+     * Start the camera.
+     */
     public void start()
     {
         cam.start();
         running = true;
     }
 
+    /**
+     * Stop the camera.
+     */
     public void stop()
     {
         cam.stop();
         running = false;
     }
 
+    /**
+     * Read the next frame from the camera.
+     */
     public void readFrame() {
         // transfer pixel data
         frame.loadPixels();
@@ -57,22 +75,42 @@ public class PS3EyeCamera implements PConstants {
             readFrame();
     }
 
+    /**
+     * Get the native ps3 eye camera reference.
+     * @return Returns the native ps3 eye camera reference.
+     */
     public PS3Eye getPS3Eye() {
         return cam;
     }
 
+    /**
+     * Get the current frame.
+     * @return Returns the current frame.
+     */
     public PImage getFrame() {
         return frame;
     }
 
+    /**
+     * Returns true, if auto frame reading is on.
+     * @return Returns if auto read is on.
+     */
     public boolean isAutoRead() {
         return autoRead;
     }
 
+    /**
+     * Set to enable auto frame reading.
+     * @param autoRead If true, auto frame reading is on.
+     */
     public void setAutoRead(boolean autoRead) {
         this.autoRead = autoRead;
     }
 
+    /**
+     * Returns true, if the camera is running.
+     * @return Value that indicates if the camera is running.
+     */
     public boolean isRunning() {
         return running;
     }
